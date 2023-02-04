495ae28718e57affc79bbdca92e2bc1f523b6dd5
Juanjo Ramos <jjramos-dev@users.noreply.github.com>
2023-02-04 21:55:51 +0100

    Hot Waxing mechanic first version

--- Depilatronics/Assets/Prefabs/HotWax.gd
+++ Depilatronics/Assets/Prefabs/HotWax.gd
@@ -2,19 +2,60 @@ extends Node2D
 
 var longo=1
 var waxing=false
+var time_from_init=-1
+
+export var time_to_fix=100
+export var max_angle=15 
+
+var old_position=null
+var direction=null
+var extended=0
 
 func _ready():
 	longo=global_position.distance_to($Tip.global_position)
 	
 func _process(delta):
+	var parar=false
+	
 	if waxing:
+#		var mouse_position=get_global_mouse_position()
+#		var direction_=Vector2(global_position.x-mouse_position.x,global_position.y-mouse_position.y).normalized()		
+#
+#		if OS.get_system_time_msecs()-time_from_init< time_to_fix or direction==null:
+#			direction=direction_
+#
+#		if rad2deg(direction.angle_to(direction_))<max_angle:
+#			$HotWax.rect_size.x=abs(mouse_position.distance_to(global_position))
+#			look_at(mouse_position)
+#		else:		
+#			end_waxing(old_position)
+#			direction=null
+#
+#		old_position=mouse_position
 		var mouse_position=get_global_mouse_position()
-		$HotWax.rect_size.x=abs(mouse_position.distance_to(global_position))
-		look_at(mouse_position)
-
+		
+		var in_body=false
+		var overlapping=$HotWax/TipCol.get_overlapping_areas()
+		for area in overlapping:
+			if area.is_in_group("Body"):
+				in_body=true
+		
+		if in_body:
+			if OS.get_system_time_msecs()-time_from_init< time_to_fix:
+				look_at(mouse_position)
+		
+			var longi=abs(mouse_position.distance_to(global_position))
+			if longi>extended:
+				$HotWax.rect_size.x=longi
+				$HotWax/TipCol.position.x=$HotWax.rect_size.x
+				$HotWax/TipCol.position.y=$HotWax.rect_size.y/2
+				extended=longi
+		
 func init_waxing(position_):
 	position=position_
 	waxing=true
-
+	time_from_init=OS.get_system_time_msecs()
+	extended=0
+	
 func end_waxing(position_):
 	waxing=false
--- Depilatronics/Scenes/FingerController.gd
+++ Depilatronics/Scenes/FingerController.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var Wax=preload("res://Assets/Prefabs/HotWax.tscn")
+var current_wax=null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +11,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("pinchar"):
+		current_wax=Wax.instance()
+		add_child(current_wax)
+		current_wax.init_waxing(get_global_mouse_position())
+	elif Input.is_action_just_released("pinchar"):
+		current_wax.end_waxing(get_global_mouse_position())
