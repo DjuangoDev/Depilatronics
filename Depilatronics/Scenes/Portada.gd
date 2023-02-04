b31d4ebf406d13a380a5f8075719f67c27ae4905
cybermito <cybermito@gmail.com>
2023-02-04 22:48:32 +0100

    Portada y Creditos con botones

---
+++ Depilatronics/Scenes/Portada.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene("res://Scenes/credits.tscn")
+	
+
+func _on_inicio_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_salir_button_pressed():
+	get_tree().quit()
